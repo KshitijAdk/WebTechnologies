e50ea7dc20bc93fea6975683d3f53d3907693167
Kshitij Adhikari <np03cs4s230250@heraldcollege.edu.np>
2023-05-22 13:28:09 +0545

    Added files to make a website

---
+++ html/website.js
@@ -0,0 +1,9 @@
+function togglePhotoList() {
+    var photoContainer = document.getElementById("photos");
+    if (photoContainer.style.display === "none") {
+      photoContainer.style.display = "block";
+    } else {
+      photoContainer.style.display = "none";
+    }
+  }
+  
